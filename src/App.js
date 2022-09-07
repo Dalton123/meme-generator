1596b265ebb0fab06c3267775c80b548423061bd
Dalton <daltonwalsh123@gmail.com>
2022-09-07 16:18:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, { useEffect } from "react";
 import Header from './components/Header';
 import Meme from './components/Meme';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "Meme Generator"
+  }, []);
+
   return (
     <div className="App">
       <Header/>
-      <Meme/>      
+      <Meme/>
     </div>
   );
 }
